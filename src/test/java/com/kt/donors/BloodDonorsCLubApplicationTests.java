8e68896d974d2cba0cd7ad69b160ce794beb2b6d
kattommy <katarzynaa.nowak@wp.pl>
2021-09-23 01:12:00 +0200

    Initial commit

---
+++ src/test/java/com/kt/donors/BloodDonorsCLubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt.donors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonorsCLubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
