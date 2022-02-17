9d1b679b593426c739d568243115e27ec296bd9a
kattommy <katarzynaa.nowak@wp.pl>
2022-02-17 11:09:33 +0100

    add user fields and methods

--- src/main/java/com/kt/donors/model/User.java
+++ src/main/java/com/kt/donors/model/User.java
@@ -1,11 +1,41 @@
 package com.kt.donors.model;
 
+import com.kt.donors.files.Donation;
+import com.kt.donors.payment.Payment;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.stereotype.Component;
+
+import javax.persistence.GeneratedValue;
+import java.util.Date;
+
 
 @Component
 @Data
 public class User {
 
+    @Id
+    @GeneratedValue
+    private long id;
+
     private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private Date attachmentDate;
+    private Payment payment;
+    private Donation donationConfirm;
+    private boolean status;
+    private String phoneNumber;
+
+
+    void changeEmail() {
+
+    }
+
+    void changePhoneNumber() {
+
+    }
+
+
 }
