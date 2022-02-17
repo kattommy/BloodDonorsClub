d464112ec630bd81383bd11a5600ee6df5ca36d6
kattommy <katarzynaa.nowak@wp.pl>
2022-02-17 11:13:33 +0100

    Add interfaces for user; Admin, SeniorMember, Member

---
+++ src/main/java/com/kt/donors/model/Admin.java
@@ -0,0 +1,9 @@
+package com.kt.donors.model;
+
+public interface Admin {
+
+    void assignTheRole(User user);
+
+    void changeUserStatus(User user);
+
+}
---
+++ src/main/java/com/kt/donors/model/Member.java
@@ -0,0 +1,16 @@
+package com.kt.donors.model;
+
+public interface Member {
+
+    void addDonationConfirm();
+
+    void showNews();
+
+    void getUserInfo();
+
+    void getDonationStatistic();
+
+    void changePassword();
+
+    void changeEmail();
+}
